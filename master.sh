cd2abea269464879453fc32097a757c311583235
ANIL DOLLOR <dollorinfotech@gmail.com>
2025-01-07 19:11:42 +0530

    Update master.sh

--- master.sh
+++ master.sh
@@ -41,3 +41,6 @@ kubectl get svc -n ingress-nginx
 
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
+helm install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --version v1.12.0 --set installCRDs=true
+kubectl get pods -n cert-manager
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.9/config/manifests/metallb-native.yaml
